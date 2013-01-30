964853fa354c1a0a9b1c4f99ddb15b90053eb281
Jorge Israel Peña <jorgepblank@gmail.com>
2013-01-30 02:14:09 -0800

    vim-like color scheme

--- zsh/zsh/alias.zsh
+++ zsh/zsh/alias.zsh
@@ -1,6 +1,3 @@
 # tmux
 alias t="tmux"
 alias ta="tmux a -t"
-
-# ls
-alias ls="ls -pG"
--- zsh/zsh/linux.zsh
+++ zsh/zsh/linux.zsh
@@ -2,5 +2,19 @@ if [[ "$OSTYPE" == linux* ]]; then
   eval $(keychain --eval --agents ssh -Q --quiet id_rsa)
   # fixes weird problem in tmux and ssh with zsh-syntax-highlighting
   export TERM=rxvt-256color
+  alias sudo='sudo '
+  alias sysd='systemd'
+  alias journ='journalctl'
+  alias ls='ls --color'
+
+  c16(){
+    x=`tput op`
+    y=`printf %76s`
+    for i in {0..16}
+    do
+      o=00$i
+      echo -e ${o:${#o}-3:3} `tput setaf $i;tput setab $i`${y// /=}$x
+    done
+  }
 fi
 
