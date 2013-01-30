b2569b2176728ed2cc72ccf824f18c627fb62bb4
Jorge Israel Peña <jorgepblank@gmail.com>
2013-01-30 00:27:46 -0800

    consistent urxvt colors; fix $TERM

--- zsh/zsh/linux.zsh
+++ zsh/zsh/linux.zsh
@@ -1,4 +1,6 @@
 if [[ "$OSTYPE" == linux* ]]; then
   eval $(keychain --eval --agents ssh -Q --quiet id_rsa)
+  # fixes weird problem in tmux and ssh with zsh-syntax-highlighting
+  export TERM=rxvt-256color
 fi
 
