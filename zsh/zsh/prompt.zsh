59e09a36426f2563b424da506ce6da2840d1a7df
Jorge Israel Peña <jorgepblank@gmail.com>
2013-02-10 16:11:51 -0500

    color path; ahead/behind by remote tracker in git

--- zsh/zsh/prompt.zsh
+++ zsh/zsh/prompt.zsh
@@ -5,10 +5,19 @@ setopt prompt_subst
 LAMBDA="%{$fg[blue]%}λ%{$reset_color%}"
 ARROW="%{$fg[blue]%}➜%{$reset_color%}"
 LEFT_ARROW="%{$fg[red]%}VI %{$reset_color%}"
+SLASH="%{$fg_bold[blue]%}/%{$reset_color%}"
+
+function color_path() {
+  print -P "${${PWD/#$HOME/~}//\//$SLASH}"
+}
+
+function vimode() {
+  echo "${${KEYMAP/vicmd/$LEFT_ARROW}/(main|viins)/}"
+}
 
 if [[ -n $SSH_CONNECTION ]]; then
   SSH="%{$fg[green]%}{%{$reset_color%}%m%{$fg[green]%}}%{$reset_color%} "
 fi
 
-PROMPT='$SSH$LAMBDA ${PWD/#$HOME/~} ${vcs_info_msg_0_}${${KEYMAP/vicmd/$LEFT_ARROW}/(main|viins)/}$ARROW '
+PROMPT='$SSH$LAMBDA ${${PWD/#$HOME/~}//\//$SLASH} ${vcs_info_msg_0_}${${KEYMAP/vicmd/$LEFT_ARROW}/(main|viins)/}$ARROW '
 
--- zsh/zsh/vcsinfo.zsh
+++ zsh/zsh/vcsinfo.zsh
@@ -11,17 +11,40 @@ COLON="%{$fg[red]%}:%{$reset_color%}"
 SPACE=" "
 DELIM=$SPACE
 
-VCS_PROMPT="$VCS_PRE%b%u%c$VCS_SUF "
+VCS_PROMPT="$VCS_PRE%b%u%c$VCS_SUF%m "
 AVCS_PROMPT="$VCS_PROMPT$AVCS_PRE%a$AVCS_SUF "
 
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' stagedstr '+'
-zstyle ':vcs_info:*' unstagedstr '*'
+zstyle ':vcs_info:*' unstagedstr 'x'
 zstyle ':vcs_info:*' formats $VCS_PROMPT
 zstyle ':vcs_info:*' actionformats $AVCS_PROMPT
 zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:git*+set-message:*' hooks git-untracked git-message
+zstyle ':vcs_info:git*+set-message:*' hooks git-aheadbehind git-untracked git-message
 precmd() { vcs_info }
+
+### git: Show +N/-N when your local branch is ahead-of or behind remote HEAD.
+# Make sure you have added misc to your 'formats':  %m
+function +vi-git-aheadbehind() {
+  local ahead behind
+  local -a gitstatus
+
+  # for git prior to 1.7
+  # ahead=$(git rev-list origin/${hook_com[branch]}..HEAD | wc -l)
+  ahead=$(git rev-list ${hook_com[branch]}@{upstream}..HEAD 2>/dev/null | wc -l)
+  (( $ahead )) && gitstatus+=( "%{$fg_bold[blue]%}+${ahead}%{$reset_color%}" )
+
+  # for git prior to 1.7
+  # behind=$(git rev-list HEAD..origin/${hook_com[branch]} | wc -l)
+  behind=$(git rev-list HEAD..${hook_com[branch]}@{upstream} 2>/dev/null | wc -l)
+  (( $behind )) && gitstatus+=( "%{$fg_bold[red]%}-${behind}%{$reset_color%}" )
+
+  hook_com[misc]+=${(j::)gitstatus}
+
+  if [[ -n ${hook_com[misc]} ]]; then
+    hook_com[misc]="$AVCS_PRE${hook_com[misc]}$AVCS_SUF"
+  fi
+}
 
 ### git: Show marker (T) if there are untracked files in repository
 # Make sure you have added staged to your 'formats':  %c
