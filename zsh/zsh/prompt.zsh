fbe6634eecc72ceb7767b371c0b9d5990222d098
Jorge Israel Peña <jorgepblank@gmail.com>
2013-02-14 11:08:39 -0500

    change ssh hostname delimiter to []'s

--- zsh/zsh/prompt.zsh
+++ zsh/zsh/prompt.zsh
@@ -16,7 +16,7 @@ function vimode() {
 }
 
 if [[ -n $SSH_CONNECTION ]]; then
-  SSH="%{$fg[green]%}{%{$reset_color%}%m%{$fg[green]%}}%{$reset_color%} "
+  SSH="%{$fg[green]%}[%{$reset_color%}%m%{$fg[green]%}]%{$reset_color%} "
 fi
 
 PROMPT='$SSH$LAMBDA $(color_path) ${vcs_info_msg_0_}$(vimode)$ARROW '
