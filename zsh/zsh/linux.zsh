e49d453bb7a0c1e27fa96e93848562215ca29184
Jorge Israel Peña <jorgepblank@gmail.com>
2013-02-17 00:34:40 -0800

    no need to set term explicitly, xft tweaks (remember: gnome overrides)

--- zsh/zsh/linux.zsh
+++ zsh/zsh/linux.zsh
@@ -1,7 +1,6 @@
 if [[ "$OSTYPE" == linux* ]]; then
   eval $(keychain --eval --agents ssh -Q --quiet id_rsa)
   # fixes weird problem in tmux and ssh with zsh-syntax-highlighting
-  export TERM=rxvt-256color
   alias sudo='sudo '
   alias sysd='systemd'
   alias sysctl='systemctl'
