bbf0b55ef18310126737b5768c38619475e0a92d
Jorge Israel Peña <jorgepblank@gmail.com>
2013-12-13 19:09:20 -0800

    keychain fix

--- zsh/zsh/linux.zsh
+++ zsh/zsh/linux.zsh
@@ -2,8 +2,9 @@ if [[ "$OSTYPE" == linux* ]]; then
   # fixes weird problem in tmux and ssh with zsh-syntax-highlighting
   alias sudo='sudo '
 
-  # only ask for ssh password on first use of ssh
+  # only ask for passphrase on first use of ssh/git
   alias ssh='eval $(keychain --eval --agents ssh -Q --quiet id_rsa) && ssh'
+  alias git='eval $(keychain --eval --agents ssh -Q --quiet id_rsa) && git'
 
   alias sysd='systemd'
   alias sysctl='systemctl'
