f32d973cc95c83011f1fb58d69126a6066c83821
Jorge Israel Peña <jorgepblank@gmail.com>
2013-12-24 17:34:49 -0800

    these binds are long overdue

--- zsh/zsh/vi.zsh
+++ zsh/zsh/vi.zsh
@@ -27,6 +27,8 @@ bindkey -M vicmd "j" down-line-or-history # history-search-forward
 bindkey -M vicmd "/" history-incremental-search-backward
 bindkey -M vicmd "?" history-incremental-search-forward
 bindkey -M vicmd "u" undo
+bindkey -M vicmd "_" beginning-of-line
+bindkey -M vicmd "g_" end-of-line
 
 zle -A .backward-kill-word vi-backward-kill-word
 zle -A .backward-delete-char vi-backward-delete-char
