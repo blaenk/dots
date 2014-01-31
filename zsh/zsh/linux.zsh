236f4a7c01950ab4d69c700cb0f64144c8c680e2
Jorge Israel Peña <jorgepblank@gmail.com>
2014-01-30 19:48:14 -0500

    package suggestions on arch

--- zsh/zsh/linux.zsh
+++ zsh/zsh/linux.zsh
@@ -17,9 +17,14 @@ if [[ "$OSTYPE" == linux* ]]; then
   eval `dircolors ~/.dircolors`
 
   export GOPATH=/home/jorge/code/go
+
+  # nvidia fix
   export __GL_SYNC_TO_VBLANK=1
   export __GL_SYNC_DISPLAY_DEVICE=DFP-0
   export __VDPAU_NVIDIA_SYNC_DISPLAY_DEVICE=DFP-0
+
+  # command not found; install pkgfile
+  [[ -e /usr/share/doc/pkgfile/command-not-found.zsh ]] && source /usr/share/doc/pkgfile/command-not-found.zsh
 
   label() {
     print -Pn "\e]2;$1\a"
