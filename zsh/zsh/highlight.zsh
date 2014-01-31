283a7e77d6457fbd219f8b24ff33b6ed77ff3d94
Jorge Israel Peña <jorgepblank@gmail.com>
2014-01-30 22:45:35 -0500

    colored manuals

--- zsh/zsh/highlight.zsh
+++ zsh/zsh/highlight.zsh
@@ -1,3 +1,19 @@
 # highlighting
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor root)
 ZSH_HIGHLIGHT_STYLES[precommand]='fg=magenta'
+
+export LESS_TERMCAP_mb=$(tput setaf 2) # green
+export LESS_TERMCAP_md=$(tput setaf 6) # cyan
+export LESS_TERMCAP_me=$(tput sgr0)
+export LESS_TERMCAP_so=$(tput bold; tput setaf 11; tput setab 7) # black on gray (vim)
+export LESS_TERMCAP_se=$(tput rmso; tput sgr0)
+export LESS_TERMCAP_us=$(tput smul; tput setaf 5) # magenta
+export LESS_TERMCAP_ue=$(tput rmul; tput sgr0)
+export LESS_TERMCAP_mr=$(tput rev)
+export LESS_TERMCAP_mh=$(tput dim)
+export LESS_TERMCAP_ZN=$(tput ssubm)
+export LESS_TERMCAP_ZV=$(tput rsubm)
+export LESS_TERMCAP_ZO=$(tput ssupm)
+export LESS_TERMCAP_ZW=$(tput rsupm)
+
+export LESS="--RAW-CONTROL-CHARS"
