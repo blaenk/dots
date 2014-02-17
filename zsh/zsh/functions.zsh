afc7cacf96bb42c43906a417ef6a323e08658860
Jorge Israel Peña <jorgepblank@gmail.com>
2014-02-16 19:19:48 -0800

    whitespace

--- zsh/zsh/functions.zsh
+++ zsh/zsh/functions.zsh
@@ -30,8 +30,8 @@ function clist(){
 
 # print numerical permissions before each item in ls
 function ls() {
-  command ls -lh --color=always $@ | \
-    awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/) \
+  command ls -lh --color=always $@ |\
+    awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/)\
          *2^(8-i));if(k)printf("%0o ",k);print}'
 }
 
