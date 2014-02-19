1ec301ab16b38e31b6d1b4bd14221379d1c9cdc4
Jorge Israel Peña <jorgepblank@gmail.com>
2014-02-18 20:39:13 -0800

    better less highlighting

--- zsh/zsh/highlight.zsh
+++ zsh/zsh/highlight.zsh
@@ -2,13 +2,21 @@
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern root)
 ZSH_HIGHLIGHT_STYLES[precommand]='fg=magenta'
 
+# to underline
+# tput smul; then tput rmul;
+
 export LESS_TERMCAP_mb=$(tput setaf 2) # green
-export LESS_TERMCAP_md=$(tput setaf 6) # cyan
+export LESS_TERMCAP_md=$(tput bold; tput setaf 6) # cyan
 export LESS_TERMCAP_me=$(tput sgr0)
+
+# search
 export LESS_TERMCAP_so=$(tput bold; tput setaf 11; tput setab 7) # black on gray (vim)
 export LESS_TERMCAP_se=$(tput rmso; tput sgr0)
-export LESS_TERMCAP_us=$(tput smul; tput setaf 5) # magenta
-export LESS_TERMCAP_ue=$(tput rmul; tput sgr0)
+
+# underline start/end
+export LESS_TERMCAP_us=$(tput bold; tput setaf 2) # green
+export LESS_TERMCAP_ue=$(tput sgr0)
+
 export LESS_TERMCAP_mr=$(tput rev)
 export LESS_TERMCAP_mh=$(tput dim)
 export LESS_TERMCAP_ZN=$(tput ssubm)
