51249b6a002b450d061097c6f9c5ba86ea71f8e8
Jorge Israel Peña <jorge.israel.p@gmail.com>
2014-04-20 22:04:38 -0400

    ubuntu command not found

--- zsh/zsh/linux.zsh
+++ zsh/zsh/linux.zsh
@@ -7,5 +7,8 @@ if [[ "$OSTYPE" == linux* ]]; then
   # command not found; install pkgfile
   [[ -e /usr/share/doc/pkgfile/command-not-found.zsh ]] &&\
     source /usr/share/doc/pkgfile/command-not-found.zsh
+
+  [[ -e /etc/zsh_command_not_found ]] &&\
+    source /etc/zsh_command_not_found
 fi
 
