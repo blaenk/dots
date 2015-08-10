6ffe36ed585de594512807c5e75b48543356fa02
Jorge Israel Peña <jorge.israel.p@gmail.com>
2015-08-09 19:34:07 -0700

    add texlive to path

--- zsh/zsh/path.zsh
+++ zsh/zsh/path.zsh
@@ -6,9 +6,13 @@ path=(~/.cabal/bin "$path[@]")
 
 # appends
 path+=(/usr/local/texlive/2013/bin/x86_64-linux)
+path+=(~/texlive/2015/bin/x86_64-linux)
 
 # prune paths that don't exist
 path=($^path(N))
 
 export GOPATH="~/code/go"
 export NODE_PATH=/usr/local/lib/node_modules:$NODE_PATH
+
+# Add ~/texlive/2015/texmf-dist/doc/info to INFOPATH.
+# Add ~/texlive/2015/texmf-dist/doc/man to MANPATH
