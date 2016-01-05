272925429ee12692e1c953c674ef08ee29d14de7
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-01-04 18:47:26 -0800

    colorize compilation mode buffer

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2192,9 +2192,17 @@ PR [a-z-+]+/\
   :config
   (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode)))
 
+(ignore-errors
+  (require 'ansi-color)
+  (defun colorize-compilation-buffer ()
+    (when (eq major-mode 'compilation-mode)
+      (ansi-color-apply-on-region compilation-filter-start (point-max))))
+
+  (add-hook 'compilation-filter-hook 'colorize-compilation-buffer))
+
 (use-package compile
   :ensure nil
-  :config
+  :init
   (setq compilation-scroll-output 'first-error)
   (setq compilation-ask-about-save nil)
   (setq compilation-set-skip-threshold 0)
