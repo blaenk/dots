832a50f4befcef73f27098d4a1c3f62d4cdab1c2
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-01-04 18:45:42 -0800

    don't think I need these

--- zsh/zsh/linux.zsh
+++ zsh/zsh/linux.zsh
@@ -1,8 +1,8 @@
 if [[ "$OSTYPE" == linux* ]]; then
   # nvidia fix
-  export __GL_SYNC_TO_VBLANK=1
-  export __GL_SYNC_DISPLAY_DEVICE=DFP-0
-  export __VDPAU_NVIDIA_SYNC_DISPLAY_DEVICE=DFP-0
+  # export __GL_SYNC_TO_VBLANK=1
+  # export __GL_SYNC_DISPLAY_DEVICE=DFP-0
+  # export __VDPAU_NVIDIA_SYNC_DISPLAY_DEVICE=DFP-0
 
   # systemctl --user env imports
   systemctl --user import-environment PATH SSH_AUTH_SOCK GTAGSCONF GTAGSLABEL VM
@@ -21,4 +21,3 @@ if [[ "$OSTYPE" == linux* ]]; then
     export TERM=xterm-256color
   fi
 fi
-
