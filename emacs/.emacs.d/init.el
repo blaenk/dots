426d7f0901742a095756dc32bb71d58965868686
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-01-11 16:12:08 -0800

    have separate custom file

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2369,16 +2369,3 @@ PR [a-z-+]+/\
 (use-package cmake-ide
   :config
   (cmake-ide-setup))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(paradox-github-token t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
