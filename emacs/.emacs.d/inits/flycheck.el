9f2ed5a850d86a0f5fc7c3b9f88457858362eb0f
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-05-07 14:17:35 -0700

    nest flycheck extensions

--- emacs/.emacs.d/inits/flycheck.el
+++ emacs/.emacs.d/inits/flycheck.el
@@ -6,20 +6,29 @@
   (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
   (setq-default flycheck-emacs-lisp-load-path 'inherit)
 
-  (add-hook 'after-init-hook 'global-flycheck-mode))
+  (add-hook 'after-init-hook 'global-flycheck-mode)
 
-(use-package flycheck-irony
-  :defer t
-  :init
-  (add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
+  (use-package flycheck-irony
+    :defer t
+    :init
+    (add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
 
-(use-package flycheck-rtags
-  :ensure nil
-  :config
-  (defun blaenk/flycheck-rtags-setup  ()
-    ;; (flycheck-select-checker 'rtags)
-    ;; (setq-local flycheck-highlighting-mode nil)
-    ;; (setq-local flycheck-check-syntax-automatically nil)
-    )
+  (use-package flycheck-rtags
+    :ensure nil
+    :config
+    (defun blaenk/flycheck-rtags-setup  ()
+      ;; (flycheck-select-checker 'rtags)
+      ;; (setq-local flycheck-highlighting-mode nil)
+      ;; (setq-local flycheck-check-syntax-automatically nil)
+      )
 
-  (add-hook 'c-mode-common-hook 'blaenk/flycheck-rtags-setup))
+    (add-hook 'c-mode-common-hook 'blaenk/flycheck-rtags-setup))
+
+  (use-package flycheck-google-cpplint
+    :init
+    (setq flycheck-c/c++-googlelint-executable "cpplint")
+
+    :config
+    (flycheck-add-next-checker
+     'c/c++-clang
+     '(warning . c/c++-googlelint))))
