15a434dbe5251fbbc68a76045b52cb6ecd5ef466
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-06-05 17:58:11 -0700

    flatten flycheck.el

--- emacs/.emacs.d/inits/conf/flycheck.el
+++ emacs/.emacs.d/inits/conf/flycheck.el
@@ -6,27 +6,28 @@
   (setq-default flycheck-emacs-lisp-load-path 'inherit)
 
   :config
-  (global-flycheck-mode)
+  (global-flycheck-mode))
 
-  (with-eval-after-load 'irony
-    (use-package flycheck-irony
-      :init
-      (add-hook 'flycheck-mode-hook #'flycheck-irony-setup)))
-
-  (with-eval-after-load 'rust
-    (use-package flycheck-rust
-      :init
-      (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
-
-  (use-package flycheck-google-cpplint
-    :disabled t
+(with-eval-after-load 'irony
+  (use-package flycheck-irony
     :init
-    (setq flycheck-c/c++-googlelint-executable "cpplint")
-    (setq flycheck-googlelint-filter "-legal")
+    (add-hook 'flycheck-mode-hook #'flycheck-irony-setup)))
 
-    :config
-    (flycheck-add-next-checker
-     'irony
-     '(warning . c/c++-googlelint))))
+(with-eval-after-load 'rust
+  (use-package flycheck-rust
+    :init
+    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
+
+(use-package flycheck-google-cpplint
+  :disabled t
+
+  :init
+  (setq flycheck-c/c++-googlelint-executable "cpplint")
+  (setq flycheck-googlelint-filter "-legal")
+
+  :config
+  (flycheck-add-next-checker
+   'irony
+   '(warning . c/c++-googlelint)))
 
 (provide 'conf/flycheck)
