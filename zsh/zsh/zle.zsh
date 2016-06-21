2c8c2e11f7d4edc06a40d6218f1d8be4ee272098
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-06-21 15:23:12 -0700

    formatting

--- zsh/zsh/zle.zsh
+++ zsh/zsh/zle.zsh
@@ -49,7 +49,7 @@ bindkey -M viins "^W" backward-kill-word
 bindkey -M viins "^A" beginning-of-line
 bindkey -M viins "^E" end-of-line
 bindkey -M viins "^H" backward-delete-char  # vi-backward-delete-char
-bindkey -M viins "^U" backward-kill-line             # vi-kill-line
+bindkey -M viins "^U" backward-kill-line    # vi-kill-line
 bindkey -M viins "^?" backward-delete-char  # vi-backward-delete-char
 
 # allow interactive incr search, ^G or ^C to exit
