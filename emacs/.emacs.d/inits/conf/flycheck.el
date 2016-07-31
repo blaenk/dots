3731fa92df267e298115d44bbffb00fd0542a313
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-07-31 15:19:46 -0700

    use the project's eslint if available

--- emacs/.emacs.d/inits/conf/flycheck.el
+++ emacs/.emacs.d/inits/conf/flycheck.el
@@ -12,6 +12,18 @@
   (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)
                 flycheck-emacs-lisp-load-path 'inherit)
 
+  (defun my-use-eslint-from-node-modules ()
+    (let* ((root (locate-dominating-file
+                  (or (buffer-file-name) default-directory)
+                  "node_modules"))
+           (eslint (and root
+                        (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                          root))))
+      (when (and eslint (file-executable-p eslint))
+        (setq-local flycheck-javascript-eslint-executable eslint))))
+
+  (add-hook 'flycheck-mode-hook #'my-use-eslint-from-node-modules)
+
   (add-hook 'after-init-hook 'global-flycheck-mode))
 
 (use-package flycheck-pos-tip
