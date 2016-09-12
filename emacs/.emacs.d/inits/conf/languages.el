907380dd0b1a4dd4d09a7618baaac44e2b2a86f0
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-09-12 14:10:12 -0700

    allow js hashbang directive

--- emacs/.emacs.d/inits/conf/languages.el
+++ emacs/.emacs.d/inits/conf/languages.el
@@ -221,6 +221,7 @@
   :init
   (setq-default js2-basic-offset 2
                 js2-mode-show-strict-warnings nil
+                js2-skip-preprocessor-directives t
                 js2-strict-trailing-comma-warning nil
                 js2-include-node-externs t
                 js2-global-externs '(
