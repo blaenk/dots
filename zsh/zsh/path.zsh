af93b449ad69d2b4732cef66e0fd0ca70b2ce0ea
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-11-25 19:04:57 -0800

    set GOPATH correctly

--- zsh/zsh/path.zsh
+++ zsh/zsh/path.zsh
@@ -14,11 +14,14 @@ path=(
 path+=(/usr/local/texlive/2013/bin/x86_64-linux)
 path+=(~/texlive/2015/bin/x86_64-linux)
 
-# prune paths that don't exist
-path=($^path(N))
+export GOPATH=${HOME}/code/go
 
-export GOPATH="~/code/go"
+path+=(${GOPATH}/bin)
+
 export NODE_PATH=/usr/local/lib/node_modules:$NODE_PATH
 
 # Add ~/texlive/2015/texmf-dist/doc/info to INFOPATH.
 # Add ~/texlive/2015/texmf-dist/doc/man to MANPATH
+
+# prune paths that don't exist
+path=($^path(N))
