5204bd0a8bc09719a4132ad240a5672013a1dc44
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-12-27 15:45:05 -0800

    update PATH for latest tex live

--- zsh/zsh/path.zsh
+++ zsh/zsh/path.zsh
@@ -11,8 +11,7 @@ path=(
     "$path[@]")
 
 # appends
-path+=(/usr/local/texlive/2013/bin/x86_64-linux)
-path+=(~/texlive/2015/bin/x86_64-linux)
+path+=(/usr/local/texlive/2016/bin/x86_64-linux)
 
 export GOPATH=${HOME}/code/go
 
@@ -22,9 +21,6 @@ export NODE_PATH=/usr/local/lib/node_modules:$NODE_PATH
 
 # rustup component add rust-src
 export RUST_SRC_PATH=${HOME}/.multirust/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src
-
-# Add ~/texlive/2015/texmf-dist/doc/info to INFOPATH.
-# Add ~/texlive/2015/texmf-dist/doc/man to MANPATH
 
 # prune paths that don't exist
 path=($^path(N))
