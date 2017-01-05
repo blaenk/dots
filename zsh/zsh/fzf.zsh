71ddb3f24a8169d9b12bc08ac86704a7017c8281
Jorge Israel Peña <jorge.israel.p@gmail.com>
2017-01-04 16:15:04 -0800

    zle widgets for dir traversal and tmux
    * M-j: fzf select a descendant to cd into
    * M-k: fzf select an ancestor to cd into
    * M-,: fzf select a window to switch to
    
    Also remove other unused tmux functions.

--- zsh/zsh/fzf.zsh
+++ zsh/zsh/fzf.zsh
@@ -10,8 +10,8 @@ _fzf_compgen_path() {
 # use fzf to select from all of the descendants
 fzf-cd-down() {
   local dir
-  dir=$(find ${1} -type d -printf '%P\n' 2> /dev/null \
-          | fzf-tmux +m) && cd "$dir"
+  dir=$(find ${1} ! -path . -type d -printf '%P\n' 2> /dev/null \
+          | fzf-tmux +m --header="cd ↓ from $PWD" --exit-0) && cd "$dir"
   zle reset-prompt
 }
 
@@ -21,7 +21,13 @@ bindkey '^[j' fzf-cd-down
 
 # use fzf to select from all of the ancestors
 fzf-cd-up() {
+  local saved
+  saved="${FZF_DEFAULT_OPTS}"
+  FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --header=\"cd ↑ from $PWD\""
+
   __enhancd::cd::builtin "$(__enhancd::arguments::dot "$2")"
+
+  FZF_DEFAULT_OPTS="${saved}"
   zle reset-prompt
 }
 
@@ -29,49 +35,10 @@ fzf-cd-up() {
 zle -N fzf-cd-up
 bindkey '^[k' fzf-cd-up
 
-# fs [FUZZY PATTERN] - Select selected tmux session
-#   - Bypass fuzzy finder if there's only one match (--select-1)
-#   - Exit if there's no match (--exit-0)
-fzf-tmux-select-session() {
-  local session
-
-  if ! tmux info &> /dev/null; then
-    echo "no tmux session available"
-    return
-  fi
-
-  session=$(tmux list-sessions -F "#{session_name}" | \
-    fzf-tmux --query="$1" --select-1 --exit-0) &&
-    [ -z "$TMUX" ] && tmux attach-session -t "$session" ||
-      tmux switch-client -t "$session"
-}
-
-# M-. to fzf sessions
-bindkey -s '^[.' 'fzf-tmux-select-session\n'
-
-# ftw - switch window
-fzf-tmux-select-window() {
-  [ -z "$TMUX" ] && return
-
-  local windows current_window target target_window
-  windows=$(tmux list-windows -F '#{window_index}: #{window_name}')
-  current_window=$(tmux display-message -p '#{window_index}: #{window_name}')
-
-  target=$(echo "$windows" | grep -v "$current_window" |
-              fzf-tmux --query="$1" --select-1 +m --reverse --exit-0) || return
-
-  target_window=$(echo $target | awk 'BEGIN{FS=":"} {print$1}')
-
-  tmux select-window -t $target_window
-}
-
-# M-, to fzf windows in current session
-bindkey -s '^[,' 'fzf-tmux-select-window\n'
-
-# ftwa - switch window all
+# select a tmux window from among all windows in every session
 fzf-tmux-select-all-window() {
   if ! tmux info &> /dev/null; then
-    echo "no tmux session available"
+    zle reset-prompt
     return
   fi
 
@@ -83,7 +50,12 @@ fzf-tmux-select-all-window() {
     windows=$(echo "$windows" | grep -v "$current_window")
   fi
 
-  target=$(echo "$windows" | fzf-tmux --query="$1" --select-1 +m --reverse --exit-0) || return
+  target=$(echo "$windows" | fzf-tmux --query="$1" --header="tmux windows" --select-1 +m --exit-0)
+
+  if [[ -z "$target" ]]; then
+    zle reset-prompt
+    return
+  fi
 
   target_session=$(echo $target | awk 'BEGIN{FS=":"} {print$1}')
   target_window=$(echo $target | awk 'BEGIN{FS=":"} {print$2}')
@@ -91,14 +63,17 @@ fzf-tmux-select-all-window() {
   tmux select-window -t "$target_session:$target_window"
 
   if [ -z "$TMUX" ]; then
-    tmux attach-session -t "$target_session"
+    BUFFER="tmux attach-session -t \"$target_session\""
   else
-    tmux switch-client -t "$target_session"
+    BUFFER="tmux switch-client -t \"$target_session\""
   fi
+
+  zle accept-line
 }
 
 # M-, to fzf all windows in every session
-bindkey -s '^[<' 'fzf-select-all-window\n'
+zle -N fzf-tmux-select-all-window
+bindkey '^[,' fzf-tmux-select-all-window
 
 if [[ -f /usr/share/fzf/key-bindings.zsh ]]; then
   source /usr/share/fzf/key-bindings.zsh
