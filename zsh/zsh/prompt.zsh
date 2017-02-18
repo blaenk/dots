34ab4d75313d60c23897271429d32dbef0b35075
Jorge Israel Peña <jorge.israel.p@gmail.com>
2017-02-17 22:11:23 -0800

    show hostname and exit status

--- zsh/zsh/prompt.zsh
+++ zsh/zsh/prompt.zsh
@@ -32,20 +32,27 @@ function p_vcs {
 
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 
+function p_hostname {
+  [[ -n $SSH_CLIENT || -n $SHOW_HOSTNAME_IN_PROMPT ]] && echo " %F{cyan}@%f %U%m%u"
+}
+
 function p_envs {
   # check for cabal sandbox in parent directories, recursively
   local cabal
   cabal=( (../)#cabal.sandbox.config(N) )
 
   local envs
-  [[ -n $SSH_CLIENT ]]  && envs+="R"
-  (( $#cabal ))         && envs+="H"
-  [[ -n $VIRTUAL_ENV ]] && envs+="P"
+  (( $#cabal ))         && envs += "H"
+  [[ -n $VIRTUAL_ENV ]] && envs += "P"
 
   [[ -n $envs ]] && echo " %F{green}[%f$envs%F{green}]%f"
 }
 
+function p_exit_code {
+  echo "%(?..%B%F{red}!%f%b )"
+}
+
 PROMPT='
-%F{blue}λ%f $(p_colored_path)$(p_envs)$(p_vcs)
-$(p_arrow) '
+%F{blue}λ%f $(p_colored_path)$(p_hostname)$(p_envs)$(p_vcs)
+$(p_exit_code)$(p_arrow) '
 
