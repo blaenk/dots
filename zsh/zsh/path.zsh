7515ce6020b58543f15cae5a372ee95eaed29e6b
Jorge Israel Peña <jorge.israel.p@gmail.com>
2017-04-25 15:41:22 -0700

    remove RLS_ROOT env var
    I believe now that rls is a rustup component this is no longer
    necessary.

--- zsh/zsh/path.zsh
+++ zsh/zsh/path.zsh
@@ -22,7 +22,5 @@ export NODE_PATH=/usr/local/lib/node_modules:$NODE_PATH
 # rustup component add rust-src
 export RUST_SRC_PATH=${HOME}/.multirust/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src
 
-export RLS_ROOT=${HOME}/code/rust/rls
-
 # prune paths that don't exist
 path=($^path(N))
