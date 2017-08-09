0a52520931c7554097a8cd3e553ad34ae7784b0a
Jorge Israel Peña <jorge.israel.p@gmail.com>
2017-08-08 18:15:23 -0700

    emacs: add indented paste commands

--- emacs/.emacs.d/inits/conf/evil.el
+++ emacs/.emacs.d/inits/conf/evil.el
@@ -198,6 +198,31 @@
     (evil-normal-state)
     (evil-visual-restore))
 
+  (evil-define-command my-evil-paste-before-and-indent
+    (count &optional register yank-handler)
+    "Pastes the latest yanked text before point
+ and gives it the same indentation as the surrounding code.
+ The return value is the yanked text."
+    (interactive "P<x>")
+    (evil-with-single-undo
+      (evil-paste-before count register yank-handler)
+      (evil-indent (evil-get-marker ?[) (evil-get-marker ?]))))
+
+  (evil-define-command my-evil-paste-after-and-indent
+    (count &optional register yank-handler)
+    "Pastes the latest yanked text behind point
+ and gives it the same indentation as the surrounding code.
+ The return value is the yanked text."
+    (interactive "P<x>")
+    (evil-with-single-undo
+      (evil-paste-after count register yank-handler)
+      (evil-indent (evil-get-marker ?[) (evil-get-marker ?]))))
+
+  (define-key evil-normal-state-map "]p" 'my-evil-paste-after-and-indent)
+  (define-key evil-normal-state-map "]P" 'my-evil-paste-before-and-indent)
+  (define-key evil-normal-state-map "[p" 'my-evil-paste-before-and-indent)
+  (define-key evil-normal-state-map "[P" 'my-evil-paste-before-and-indent)
+
   (defun my-evil-open-in-between ()
     (interactive)
 
