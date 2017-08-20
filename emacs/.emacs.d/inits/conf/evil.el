7b8efe8b9a02fbe58db12dddf6491a2d6b40e024
Jorge Israel Peña <jorge.israel.p@gmail.com>
2017-08-19 19:04:31 -0700

    emacs: update paste-with-indent implementation

--- emacs/.emacs.d/inits/conf/evil.el
+++ emacs/.emacs.d/inits/conf/evil.el
@@ -233,22 +233,24 @@
   (evil-define-command my-evil-paste-before-and-indent
     (count &optional register yank-handler)
     "Pastes the latest yanked text before point
- and gives it the same indentation as the surrounding code.
- The return value is the yanked text."
+and gives it the same indentation as the surrounding code.
+The return value is the yanked text."
     (interactive "P<x>")
     (evil-with-single-undo
-      (evil-paste-before count register yank-handler)
-      (evil-indent (evil-get-marker ?[) (evil-get-marker ?]))))
+      (let ((text (evil-paste-before count register yank-handler)))
+        (evil-indent (evil-get-marker ?[) (evil-get-marker ?]))
+        text)))
 
   (evil-define-command my-evil-paste-after-and-indent
     (count &optional register yank-handler)
     "Pastes the latest yanked text behind point
- and gives it the same indentation as the surrounding code.
- The return value is the yanked text."
+and gives it the same indentation as the surrounding code.
+The return value is the yanked text."
     (interactive "P<x>")
     (evil-with-single-undo
-      (evil-paste-after count register yank-handler)
-      (evil-indent (evil-get-marker ?[) (evil-get-marker ?]))))
+      (let ((text (evil-paste-after count register yank-handler)))
+        (evil-indent (evil-get-marker ?[) (evil-get-marker ?]))
+        text)))
 
   (evil-define-operator my-evil-yank-without-indentation (beg end type)
     "Saves the characters in motion into the kill-ring."
