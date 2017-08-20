e889d04f67c3b0ab15cf9f4a17274c8e6a4e745e
Jorge Israel Peña <jorge.israel.p@gmail.com>
2017-08-20 00:21:04 -0700

    emacs: refactor languages.el

--- emacs/.emacs.d/inits/conf/languages.el
+++ emacs/.emacs.d/inits/conf/languages.el
@@ -11,10 +11,10 @@
   :init
   (setq ahk-indentation 2)
 
-  (defun my-ahk-mode-hook()
-    (run-mode-hooks 'prog-mode-hook))
+  (defun my--ahk-mode-hook()
+    (run-mode-hooks #'prog-mode-hook))
 
-  (add-hook 'ahk-mode-hook 'my-ahk-mode-hook))
+  (add-hook 'ahk-mode-hook #'my--ahk-mode-hook))
 
 (use-package dockerfile-mode :defer t)
 
@@ -33,8 +33,6 @@
 (use-package haskell-mode :defer t)
 
 (use-package markdown-mode
-  :defer t
-
   :mode
   (("\\.md\\'" . gfm-mode)
    ("\\.markdown\\'" . gfm-mode))
@@ -50,9 +48,15 @@
     "t t" 'orgtbl-mode)
 
   :init
+  ;; TODO
+  ;; Turn this into an advice instead?
+  ;; Or turn this into a macro that advises or creates functions to re-use? This
+  ;; has become a pretty common task.
   (defun my-insert-gfm-code-block ()
+    "Insert a GFM code block, then enter evil insert state."
     (interactive)
-    (call-interactively 'markdown-insert-gfm-code-block)
+
+    (call-interactively #'markdown-insert-gfm-code-block)
     (evil-insert-state))
 
   (setq markdown-enable-math t
@@ -61,7 +65,7 @@
         markdown-use-pandoc-style-yaml-metadata t
         markdown-gfm-additional-languages '("cpp" "elisp" "postgresql"))
 
-  (defun my-markdown-mode-hook ()
+  (defun my--markdown-mode-hook ()
     (setq-local word-wrap t)
     (setq-local outline-regexp "^\\(?:\\(#+\\)[ \t]+\\(.*?\\)[ \t]*\\(#*\\)\\)$")
 
@@ -70,7 +74,7 @@
       (push '(?i . ("_" . "_")) evil-surround-pairs-alist)
       (push '(?p . ("<span class=\"path\">" . "</span>")) evil-surround-pairs-alist)))
 
-  (add-hook 'markdown-mode-hook #'my-markdown-mode-hook)
+  (add-hook 'markdown-mode-hook #'my--markdown-mode-hook)
   (add-hook 'markdown-mode-hook #'olivetti-mode)
   (add-hook 'markdown-mode-hook #'outline-minor-mode)
   (add-hook 'markdown-mode-hook #'flyspell-mode)
@@ -78,11 +82,12 @@
   (add-hook 'markdown-mode-hook #'whitespace-mode))
 
 (use-package markdown-edit-indirect
-  :defer t
-
   :general
   (:keymaps 'markdown-mode-map
-   "C-'" 'markdown-edit-indirect))
+   "C-'" 'markdown-edit-indirect)
+
+  (my-map
+    "m e" 'markdown-edit-indirect))
 
 (use-package yaml-mode
   :defer t
@@ -95,19 +100,19 @@
   :defer t
 
   :init
-  (add-hook 'ruby-mode-hook 'yard-mode))
+  (add-hook 'ruby-mode-hook #'yard-mode))
 
 (use-package robe
   :defer t
 
   :init
-  (add-hook 'ruby-mode-hook 'robe-mode))
+  (add-hook 'ruby-mode-hook #'robe-mode))
 
 (use-package inf-ruby
   :defer t
 
   :init
-  (add-hook 'ruby-mode-hook 'inf-ruby-minor-mode))
+  (add-hook 'ruby-mode-hook #'inf-ruby-minor-mode))
 
 (use-package go-guru :defer t)
 
@@ -120,7 +125,7 @@
   (setq gofmt-command "goimports")
 
   (add-hook 'go-mode-hook #'subword-mode)
-  (add-hook 'before-save-hook 'gofmt-before-save))
+  (add-hook 'before-save-hook #'gofmt-before-save))
 
 (use-package go-eldoc
   :defer t
@@ -136,11 +141,11 @@
   :mode "\\.sass\\'"
 
   :init
-  (defun my-scss-hook ()
+  (defun my--scss-hook ()
     (setq-local comment-end "")
     (setq-local comment-start "//"))
 
-  (add-hook 'scss-mode-hook #'my-scss-hook))
+  (add-hook 'scss-mode-hook #'my--scss-hook))
 
 (use-package css-eldoc
   :defer t
@@ -201,9 +206,6 @@
                                      "specify"
                                      ))
 
-  (add-hook 'js2-mode-hook #'subword-mode)
-  (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
-
   (defun my--route-expression (method)
     `(,(upcase method)
       ,(concat
@@ -212,7 +214,7 @@
         "\\s-*([\"']\\(.+\\)[\"']\\s-*,.*")
       1))
 
-  (defvar my-route-imenu-expressions
+  (defvar my--route-imenu-expressions
     (-map #'my--route-expression
           '("all"
             "delete"
@@ -234,7 +236,7 @@
         "\\(\.skip\\|\.only\\)?\\s-*([\"']\\(.+\\)[\"']\\s-*,.*")
       2))
 
-  (defvar my-mocha-imenu-expressions
+  (defvar my--mocha-imenu-expressions
     (-map #'my--mocha-expression
           '("after"
             "afterEach"
@@ -244,38 +246,42 @@
             "before"
             "describe")))
 
-  (defun my-js2-imenu-index-function ()
+  (defun my--js2-imenu-index-function ()
     ;; a submenu can be created as:
     ;; (list (cons "Menu Title" (imenu--generic-function expressions)))
     (let ((js2-imenu-index (js2-mode-create-imenu-index))
-          (mocha-index (imenu--generic-function my-mocha-imenu-expressions))
-          (route-index (imenu--generic-function my-route-imenu-expressions)))
+          (mocha-index (imenu--generic-function my--mocha-imenu-expressions))
+          (route-index (imenu--generic-function my--route-imenu-expressions)))
       (-concat js2-imenu-index mocha-index route-index)))
 
-  (defun my-js2-imenu-extras-hook ()
-    (setq-local imenu-create-index-function #'my-js2-imenu-index-function))
+  (defun my--js2-imenu-extras-hook ()
+    (setq-local imenu-create-index-function #'my--js2-imenu-index-function))
 
-  (add-hook 'js2-imenu-extras-mode-hook #'my-js2-imenu-extras-hook)
+  (add-hook 'js2-imenu-extras-mode-hook #'my--js2-imenu-extras-hook)
+
+  (add-hook 'js2-mode-hook #'subword-mode)
+  (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
 
   :config
-  (defun my-js2-comment-line-break (&optional soft)
+  (defun my--js2-comment-line-break (&optional soft)
     (if (nth 4 (syntax-ppss))
         (js2-line-break soft)
       (comment-indent-new-line soft)))
 
-  (defun my-js2-hook ()
-    (setq-local comment-line-break-function #'my-js2-comment-line-break))
+  (defun my--js2-hook ()
+    (setq-local comment-line-break-function #'my--js2-comment-line-break))
 
-  (add-hook 'js2-mode-hook #'my-js2-hook))
+  (add-hook 'js2-mode-hook #'my--js2-hook))
 
 (use-package rjsx-mode
   :mode "/\\(components\\|containers\\)/.+\\.js")
 
 (use-package prettier-js
-  :defer t
-
   :general
   (:keymaps 'js2-mode-map
+   "C-c C-f" 'prettier-js)
+
+  (my-map :keymaps 'js2-mode-map
    "m f" 'prettier-js))
 
 (use-package json-mode
@@ -314,7 +320,7 @@
 
   :config
   (with-eval-after-load 'evil
-    (add-hook 'rtags-jump-hook 'evil--jumps-push)))
+    (add-hook 'rtags-jump-hook #'evil--jumps-push)))
 
 (use-package modern-cpp-font-lock
   :defer t
@@ -338,11 +344,11 @@
   :defer t
 
   :init
-  (defun my-rust-hook ()
-    (set (make-local-variable 'compile-command) "cargo build")
+  (defun my--rust-hook ()
+    (setq-local compile-command "cargo build")
     (helm-gtags-mode))
 
-  (add-hook 'rust-mode-hook #'my-rust-hook))
+  (add-hook 'rust-mode-hook #'my--rust-hook))
 
 (use-package rust-playground :defer t)
 
@@ -369,9 +375,6 @@
          ("\\.handlebars\\'" . web-mode))
 
   :init
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2)
   (setq web-mode-enable-current-element-highlight t))
 
 (use-package emmet-mode
@@ -382,11 +385,11 @@
    "C-S-j" 'my-emmet-preview)
 
   :init
-  (defun my-emmet-jsx ()
+  (defun my--emmet-jsx ()
     (setq-local emmet-expand-jsx-className? t))
 
-  (add-hook 'rjsx-mode-hook #'my-emmet-jsx)
-  (add-hook 'js2-jsx-mode-hook #'my-emmet-jsx)
+  (add-hook 'rjsx-mode-hook #'my--emmet-jsx)
+  (add-hook 'js2-jsx-mode-hook #'my--emmet-jsx)
 
   (add-hook 'js2-mode-hook #'emmet-mode)
   (add-hook 'sgml-mode-hook #'emmet-mode)
@@ -395,12 +398,13 @@
 
   :config
   (defun my-emmet-preview ()
+    "Preview the emmet expression."
     (interactive)
+
     (emmet-expand-line '(4))))
 
 (use-package tex-site
   :ensure auctex
-  :defer t
 
   :general
   (:keymaps 'LaTeX-mode-map
@@ -410,22 +414,6 @@
     "m c" 'my-latex-make-and-preview)
 
   :init
-  (define-advice TeX-command-sentinel
-      (:after (process msg) show-output-window-on-error)
-    (if (not (eq (process-exit-status process) 0))
-        (TeX-recenter-output-buffer nil)))
-
-  ;; Offer to save the file if there are changes, then build the file with Make.
-  (defun my-latex-make-and-preview ()
-    (interactive)
-
-    (if (and (buffer-modified-p)
-             (y-or-n-p (concat "Save file " buffer-file-name "? ")))
-        (save-buffer))
-
-    (flet ((TeX-process-check (&rest IGNORE) nil))
-      (TeX-command "Make" 'TeX-master-file 0)))
-
   ;; Introduce two expansion variables:
   ;;   %projectile-root is self-explanatory
   ;;   %pdf-path is the path to the backing file with a .pdf extension
@@ -446,6 +434,22 @@
         TeX-parse-self t
         TeX-view-program-selection '((output-pdf "PDF Viewer"))
         TeX-view-program-list '(("PDF Viewer" "open %pdf-path")))
+
+  (define-advice TeX-command-sentinel
+      (:after (process msg) show-output-window-on-error)
+    (if (not (eq (process-exit-status process) 0))
+        (TeX-recenter-output-buffer nil)))
+
+  (defun my-latex-make-and-preview ()
+    "Offer to save the file if there are changes, then build the file with Make."
+    (interactive)
+
+    (if (and (buffer-modified-p)
+             (y-or-n-p (concat "Save file " buffer-file-name "? ")))
+        (save-buffer))
+
+    (flet ((TeX-process-check (&rest IGNORE) nil))
+      (TeX-command "Make" 'TeX-master-file 0)))
 
   (add-hook 'LaTeX-mode-hook #'olivetti-mode)
   (add-hook 'LaTeX-mode-hook #'flyspell-mode)
