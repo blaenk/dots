745eec8d9311b7a74926f264f1e36a14fc6c3d7e
Jorge Israel Peña <jorge.israel.p@gmail.com>
2017-08-24 15:21:08 -0700

    emacs: merge helm-regexp configs

--- emacs/.emacs.d/inits/conf/helm.el
+++ emacs/.emacs.d/inits/conf/helm.el
@@ -190,8 +190,14 @@ It can clash with the colors being shown."
   :ensure nil
   :defer t
 
-  :init
-  (setq helm-moccur-show-buffer-fontification t))
+  :config
+  ;; Make helm-occur auto-enable follow-mode, acting more like swiper.
+  ;; see https://github.com/emacs-helm/helm/issues/530#issuecomment-195350607
+  (setq helm-moccur-show-buffer-fontification t
+        helm-source-occur
+        (helm-make-source "Occur" 'helm-source-multi-occur
+          :follow 1)))
+
 
 (use-package helm-font
   :ensure nil
@@ -200,17 +206,6 @@ It can clash with the colors being shown."
   :general
   (my-map
     "i c" 'helm-ucs))
-
-(use-package helm-regexp
-  :ensure nil
-  :defer t
-
-  :config
-  ;; Make helm-occur auto-enable follow-mode, acting more like swiper.
-  ;; see https://github.com/emacs-helm/helm/issues/530#issuecomment-195350607
-  (setq helm-source-occur
-        (helm-make-source "Occur" 'helm-source-multi-occur
-          :follow 1)))
 
 (use-package helm-semantic
   :ensure nil
