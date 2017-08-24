e8f4006c74f108b4b2bd5dc90dd6b655eb7b9232
Jorge Israel Peña <jorge.israel.p@gmail.com>
2017-08-24 09:18:54 -0700

    emacs: major refactor of mode-line.el
    Mainly consistent naming, privatizing, etc.

--- emacs/.emacs.d/inits/conf/mode-line.el
+++ emacs/.emacs.d/inits/conf/mode-line.el
@@ -23,36 +23,35 @@
 ;; work around this by locally binding the format strings.
 
 ;; For the header-line show the tag name as well.
-(defun my--eyebrowse-indicator-header-line ()
+(defun my--header-line-eyebrowse-component ()
   (when (and (my--is-selected-window-p) header-line-format)
     (let ((eyebrowse-slot-format " %s ")
           (eyebrowse-tagged-slot-format " %s:%t "))
       (eyebrowse-mode-line-indicator))))
 
 ;; For the mode-line just show the slot number.
-(defun my--eyebrowse-indicator-mode-line ()
+(defun my--mode-line-eyebrowse-component ()
   (when (and (my--is-selected-window-p) (not header-line-format))
     (let ((eyebrowse-slot-format " %s ")
           (eyebrowse-tagged-slot-format " %s "))
       (eyebrowse-mode-line-indicator))))
 
+(defun my--mode-line-which-func-component ()
+  (propertize
+   (concat " λ " (gethash (selected-window) which-func-table) " ")
+   'face 'mode-line-which-function-face))
+
 (defconst my--header-line-left
   `(
     (which-function-mode
-     (:eval
-      (propertize
-       (concat " λ " (gethash (selected-window) which-func-table) " ")
-       'face 'mode-line-which-function-face)
-       ))
+     (:eval (my--mode-line-which-func-component)))
     ))
 
 (defconst my--header-line-right
   `(
-    (:propertize
-     (:eval (s-wrap mode-name " "))
-     face mode-line-mode-name-face)
+    (:propertize (:eval (s-wrap mode-name " ")) face mode-line-mode-name-face)
     (eyebrowse-mode
-     (:eval (my--eyebrowse-indicator-header-line)))
+     (:eval (my--header-line-eyebrowse-component)))
     ))
 
 (defconst my--header-line-template
@@ -73,6 +72,7 @@
 (my-map
   "t h" 'my-toggle-header-line)
 
+;; Currently-selected window code.
 (defvar my--selected-window (frame-selected-window)
   "The currently-selected window.")
 
@@ -91,10 +91,14 @@
 
 (define-advice handle-switch-frame
     (:after (event) save-selected-window-after-switch-frame)
+  "Set the newly selected window after switching frames."
+
   (my--save-selected-window))
 
 (define-advice select-window
     (:after (window &optional norecord) save-selected-window)
+  "Set the newly selected window."
+
   (my--save-selected-window))
 
 (defun my--is-selected-window-p ()
@@ -110,39 +114,39 @@
     (fontawesome "cloud")
     "☁"))
 
-(defun my--is-evil-on-p ()
+(defun my--is-vim-state-enabled-p ()
   (and (or (bound-and-true-p evil-mode)
            (bound-and-true-p evil-local-mode))
        (not (evil-emacs-state-p))))
 
-(defun my--evil-indicator ()
-  (let* ((is-evil (my--is-evil-on-p))
-         (indicator (if is-evil "V" "E"))
-         (face (if is-evil
+(defun my--mode-line-evil-component ()
+  (let* ((is-vim (my--is-vim-state-enabled-p))
+         (indicator (if is-vim "V" "E"))
+         (face (if is-vim
                    'mode-line-evil-mode-indicator-face
                  'mode-line-emacs-mode-indicator-face)))
     (propertize (s-wrap indicator " ") 'face face)))
 
-(defun my--remote-mode-line ()
+(defun my--mode-line-remote-component ()
   (when (file-remote-p buffer-file-name)
     (let ((host (tramp-file-name-host (tramp-dissect-file-name buffer-file-name))))
       (s-wrap (concat my--cloud-icon " " host) " "))))
 
-(defun my--format-error (count face)
+(defun my--mode-line-format-error (count face)
   (when (and count (> count 0))
     (propertize (s-wrap (number-to-string count) " ") 'face face)))
 
-(defun my--format-flycheck-errors ()
+(defun my--mode-line-format-flycheck-errors ()
   (if (flycheck-has-current-errors-p)
       (let-alist (flycheck-count-errors flycheck-current-errors)
         (let* ((error-counts (flycheck-count-errors flycheck-current-errors)))
           (concat
-           (my--format-error .info 'mode-line-flycheck-infos-face)
-           (my--format-error .warning 'mode-line-flycheck-warnings-face)
-           (my--format-error .error 'mode-line-flycheck-errors-face))))
+           (my--mode-line-format-error .info 'mode-line-flycheck-infos-face)
+           (my--mode-line-format-error .warning 'mode-line-flycheck-warnings-face)
+           (my--mode-line-format-error .error 'mode-line-flycheck-errors-face))))
     (propertize " ✔ " 'face 'mode-line-flycheck-no-errors-face)))
 
-(defun my--flycheck-mode-line ()
+(defun my--mode-line-flycheck-component ()
   (pcase flycheck-last-status-change
     (`not-checked nil)
     (`no-checker nil)
@@ -150,16 +154,16 @@
     (`errored (propertize " ‼ " 'face 'mode-line-flycheck-errors-face))
     (`interrupted (propertize " ⊣ " 'face 'mode-line-flycheck-errors-face))
     (`running (propertize " … " 'face 'mode-line-flycheck-checking-face))
-    (`finished (my--format-flycheck-errors))))
+    (`finished (my--mode-line-format-flycheck-errors))))
 
-(defun my--compilation-mode-line ()
+(defun my--mode-line-compile-component ()
   (when (derived-mode-p 'compilation-mode)
     (concat
-     (my--format-error compilation-num-infos-found 'mode-line-flycheck-infos-face)
-     (my--format-error compilation-num-warnings-found 'mode-line-flycheck-warnings-face)
-     (my--format-error compilation-num-errors-found 'mode-line-flycheck-errors-face))))
+     (my--mode-line-format-error compilation-num-infos-found 'mode-line-flycheck-infos-face)
+     (my--mode-line-format-error compilation-num-warnings-found 'mode-line-flycheck-warnings-face)
+     (my--mode-line-format-error compilation-num-errors-found 'mode-line-flycheck-errors-face))))
 
-(defun my--vc-git-status ()
+(defun my--mode-line-git-status-component ()
   (-when-let* ((_ buffer-file-name)
                (rev (vc-working-revision buffer-file-name 'Git))
                (state
@@ -179,32 +183,32 @@
                ((label . face) state))
     (propertize (s-wrap label " ") 'face face)))
 
-(defvar-local my--vc-git-status-cache ""
+(defvar-local my--mode-line-git-status-component-cache ""
   "Cache the vc-git-status response")
 
-(defun my--vc-git-mode ()
+(defun my--mode-line-git-branch-component ()
   (-when-let* ((_ buffer-file-name)
                (rev (vc-working-revision buffer-file-name 'Git))
                (disp-rev (or (vc-git--symbolic-ref buffer-file-name)
                              (substring rev 0 7))))
     (concat (propertize (s-wrap disp-rev " ") 'face 'mode-line-branch-face))))
 
-(defvar-local my--vc-git-mode-cache ""
+(defvar-local my--mode-line-git-branch-component-cache ""
   "Cache the vc-git-mode response")
 
-(defun my--cache-vc-info ()
-  (setq-local my--vc-git-status-cache (my--vc-git-status))
-  (setq-local my--vc-git-mode-cache (my--vc-git-mode)))
+(defun my--mode-line-cache-git-info ()
+  (setq-local my--mode-line-git-status-component-cache (my--mode-line-git-status-component))
+  (setq-local my--mode-line-git-branch-component-cache (my--mode-line-git-branch-component)))
 
-(add-hook 'find-file-hook #'my--cache-vc-info)
-(add-hook 'after-revert-hook #'my--cache-vc-info)
+(add-hook 'find-file-hook #'my--mode-line-cache-git-info)
+(add-hook 'after-revert-hook #'my--mode-line-cache-git-info)
 
-(defun my--is-modified-p ()
+(defun my--is-buffer-modified-p ()
   (and (not buffer-read-only) (buffer-modified-p)))
 
 ;; Construct the buffer identifier for a buffer backed by a file. This is done
 ;; by combining: dirname/ + filename, each propertized separately.
-(defun my--file-identification (path &optional max-width)
+(defun my--mode-line-file-identifier (path &optional max-width)
   (let* ((path (if (file-remote-p buffer-file-name)
                    (tramp-file-name-localname (tramp-dissect-file-name buffer-file-name))
                  path))
@@ -221,9 +225,9 @@
 
 ;; Construct the buffer identifier for a regular, simple buffer that is not
 ;; backed by a file nor remote.
-(defun my--regular-identification (&optional max-width)
+(defun my--mode-line-buffer-identifier (&optional max-width)
   (if buffer-file-name
-      (my--file-identification buffer-file-name max-width)
+      (my--mode-line-file-identifier buffer-file-name max-width)
     (propertize "%b" 'face 'mode-line-buffer-id)))
 
 (defun my--frame-title-format ()
@@ -243,37 +247,37 @@
          (f-relative buffer-file-name (projectile-project-root))
        (buffer-name))))
 
-   (t (my--regular-identification))))
+   (t (my--mode-line-buffer-identifier))))
 
-(defun my--buffer-identification (&optional max-width)
+(defun my--mode-line-buffer-identifier-component (&optional max-width)
   (concat
    " "
    (cond
     ((and buffer-file-name (file-remote-p buffer-file-name))
-     (my--file-identification
+     (my--mode-line-file-identifier
       (tramp-file-name-localname (tramp-dissect-file-name buffer-file-name))
       max-width))
 
     ((and (featurep 'projectile) (and buffer-file-name (projectile-project-p)))
-     (my--file-identification
+     (my--mode-line-file-identifier
       (f-relative buffer-file-name (projectile-project-root))
       max-width))
 
-    (t (my--regular-identification max-width)))
+    (t (my--mode-line-buffer-identifier max-width)))
    " "))
 
-(defun my--column-number--linum ()
+(defun my--mode-line-column-component--linum ()
   (propertize "%4c " 'face 'mode-line-column-face))
 
-(defun my--column-number--native ()
+(defun my--mode-line-column-component--native ()
   (propertize (format "%%%dc " (+ (line-number-display-width) 2))
               'face 'mode-line-column-face))
 
-(fset 'my--column-number (if (< emacs-major-version 26)
-                             'my--column-number--linum
-                           'my--column-number--native))
+(fset 'my--mode-line-column-component (if (< emacs-major-version 26)
+                             'my--mode-line-column-component--linum
+                           'my--mode-line-column-component--native))
 
-(defun my--mode-line-project-indicator ()
+(defun my--mode-line-project-component ()
   (when (and (not (file-remote-p buffer-file-name))
              (featurep 'projectile) (projectile-project-p))
     (propertize
@@ -283,38 +287,38 @@
 (defconst my--mode-line-left
       `(
         (my--display-column-number
-         (:eval (my--column-number)))
+         (:eval (my--mode-line-column-component)))
         (anzu-mode
          (:propertize
           (:eval (anzu--update-mode-line))
           face mode-line-anzu-face))
-        (:eval (my--evil-indicator))
+        (:eval (my--mode-line-evil-component))
         (buffer-file-name
-         (:eval (my--mode-line-project-indicator)))
+         (:eval (my--mode-line-project-component)))
         (buffer-file-name
          (:propertize
-          (:eval (my--remote-mode-line))
+          (:eval (my--mode-line-remote-component))
           face mode-line-remote-face))
         ))
 
 (defconst my--mode-line-right
       `(
         (:propertize
-         (:eval (when (my--is-modified-p) " + "))
+         (:eval (when (my--is-buffer-modified-p) " + "))
          face mode-line-modified-face)
         (edebug-mode
          (:propertize " DBG " face mode-line-edebug-face))
         (global-flycheck-mode
-         (:eval (my--flycheck-mode-line)))
+         (:eval (my--mode-line-flycheck-component)))
         (buffer-read-only
          (:propertize
           (:eval (s-wrap my--lock-icon " "))
           face mode-line-read-only-face))
-        (:eval (my--compilation-mode-line))
-        (:eval my--vc-git-status-cache)
-        (:eval my--vc-git-mode-cache)
+        (:eval (my--mode-line-compile-component))
+        (:eval my--mode-line-git-status-component-cache)
+        (:eval my--mode-line-git-branch-component-cache)
         (eyebrowse-mode
-         (:eval (my--eyebrowse-indicator-mode-line)))
+         (:eval (my--mode-line-eyebrowse-component)))
         ))
 
 (defconst my--default-mode-line-format mode-line-format
@@ -324,7 +328,7 @@
   `(:eval (my--render-mode-line
            (format-mode-line my--mode-line-left)
            (format-mode-line my--mode-line-right)
-           #'my--buffer-identification))
+           #'my--mode-line-buffer-identifier-component))
   "My custom mode-line-format.")
 
 (defun my-toggle-custom-mode-line (arg)
