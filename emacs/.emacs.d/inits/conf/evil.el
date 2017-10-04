20e6d5458debf79168179bb89fdb1309e709ed27
Jorge Israel Peña <jorge.israel.p@gmail.com>
2017-10-03 20:06:23 -0700

    emacs: remove evil-goggles-pulse

--- emacs/.emacs.d/inits/conf/evil.el
+++ emacs/.emacs.d/inits/conf/evil.el
@@ -466,7 +466,6 @@ The return value is the yanked text."
 
   :init
   (setq evil-goggles-duration 0.100
-        evil-goggles-pulse t
 
         ;; to disable the hint when pasting:
         ;; evil-goggles-enable-paste nil
