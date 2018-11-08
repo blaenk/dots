fe72911e57c95da1d6d3d9f799e9e8d7d20467c9
Jorge Israel Peña <jorge.israel.p@gmail.com>
2018-11-08 14:22:29 -0800

    zsh: use brew's command-not-found on macos

--- zsh/zsh/brew.zsh
+++ zsh/zsh/brew.zsh
@@ -2,6 +2,13 @@ if ! command_exists brew; then
   return 0
 fi
 
+if ! brew command command-not-found-init > /dev/null 2>&1; then
+  brew tap homebrew/command-not-found
+fi
+
+eval "$(brew command-not-found-init)"
+
+# Override certain macos utilities with the GNU coreutil versions
 hash ls=/usr/local/bin/gls
 hash wc=/usr/local/bin/gwc
 hash rm=/usr/local/bin/grm
