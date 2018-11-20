01538b893bb73be74ac8e5a29abb90822f797c70
Jorge Israel Peña <jorge.israel.p@gmail.com>
2018-11-20 12:27:10 -0800

    zsh: clean up PATH configuration

--- zsh/zsh/path.zsh
+++ zsh/zsh/path.zsh
@@ -1,24 +1,8 @@
 typeset -U path
 
-# prepends
-path=(
-    ~/.cargo/bin
-    ~/.rbenv/bin
-    ~/.cabal/bin
-    "$path[@]")
-
-# appends
-path+=(/usr/local/texlive/2018/bin/x86_64-linux)
-
 export GOPATH=${HOME}/code/go
 
-path+=(${GOPATH}/bin)
-
 export NODE_PATH=/usr/local/lib/node_modules:$NODE_PATH
-
-if [[ "$MACOS" ]]; then
- path+=(/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin)
-fi
 
 if command_exists rustc; then
   # rustup component add rust-src
@@ -27,6 +11,22 @@ if command_exists rustc; then
   # rust libraries
   export LD_LIBRARY_PATH=$(rustc --print sysroot)/lib:$LD_LIBRARY_PATH
 fi
+
+mkdir -p ~/bin
+
+path=(
+  ~/bin
+  "$path[@]"
+
+  ~/.cargo/bin
+  ~/.rbenv/bin
+  ~/.cabal/bin
+  ~/.rvm/bin
+
+  /usr/local/texlive/2018/bin/x86_64-linux
+  ${GOPATH}/bin
+  /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin
+)
 
 # prune paths that don't exist
 path=($^path(N))
