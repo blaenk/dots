11b540143291bb34b8866fe9d2e8303b2f446f76
Jorge Israel Peña <jorge.israel.p@gmail.com>
2018-12-04 15:05:46 -0800

    emacs: use transparent titlebar on macOS
    This requires the emacs-plus brew formula

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -21,7 +21,12 @@
     (set-fontset-font t 'symbol
                       (font-spec :family "Apple Color Emoji") nil 'prepend)
     (set-fontset-font t 'symbol
-                      (font-spec :family "Apple Symbols") nil 'prepend))))
+                      (font-spec :family "Apple Symbols") nil 'prepend)
+
+    ;; Make the titlebar transparent, effectively making it the same color as
+    ;; the background.
+    (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+    (add-to-list 'default-frame-alist '(ns-appearance . light)))))
 
 (my--set-frame-options nil)
 
