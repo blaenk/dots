338fd55fd000a675744580db05140dd7af920ad9
Jorge Israel Peña <jorge.israel.p@gmail.com>
2022-03-31 02:38:12 -0700

    emacs: don't bother creating cache dir

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -92,9 +92,6 @@
 
 (add-to-list 'custom-theme-load-path (my-inits-dir "conf/"))
 
-(when (not (file-exists-p (my-cache-dir)))
-  (make-directory (my-cache-dir)))
-
 (let* ((auto-save-dir (my-cache-dir "autosaves/")))
   (setq auto-save-list-file-prefix (expand-file-name "saves-" auto-save-dir)
         auto-save-file-name-transforms `((".*" ,auto-save-dir t))))
