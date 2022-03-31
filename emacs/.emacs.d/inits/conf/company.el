2bef03c7255fd10a7c170f4c7b3de80cd131d397
Jorge Israel Peña <jorge.israel.p@gmail.com>
2022-03-31 02:48:06 -0700

    emacs: declutter company config

--- emacs/.emacs.d/inits/conf/company.el
+++ emacs/.emacs.d/inits/conf/company.el
@@ -18,9 +18,6 @@
    "C-o" 'company-show-location
    "C-/" 'company-filter-candidates)
 
-  (:keymaps 'yas-minor-mode-map
-   "M-N" 'my-company-yasnippet)
-
   :init
   (setq company-minimum-prefix-length 1
         company-selection-wrap-around t
@@ -31,53 +28,6 @@
         company-dabbrev-code-other-buffers 'code
         company-dabbrev-code-everywhere t)
 
-  (defun my-company-yasnippet ()
-    "Expand partial snippet or choose a snippet with company.
-
-If a region is active, it'll be used to \"wrap\" the selection."
-    (interactive)
-
-    ;; If there region is active or there's nothing to expand, use completing
-    ;; read to select the snippet. Otherwise expand.
-    (if (or (region-active-p)
-            (not (yas--maybe-expand-key-filter 'yas-expand)))
-        (call-interactively #'company-yasnippet)
-      (yas-expand)))
-
   (add-hook 'after-init-hook #'global-company-mode))
-
-(use-package company-quickhelp
-  :general
-  (:keymaps 'company-active-map
-   "M-h" 'company-quickhelp-manual-begin)
-
-  :init
-  (setq company-quickhelp-delay nil)
-
-  :config
-  (company-quickhelp-mode 1))
-
-(use-package company-math
-  :defer t
-
-  :init
-  (defun my--company-math ()
-    (add-to-list 'company-backends 'company-math-symbols-unicode t)
-    (add-to-list 'company-backends 'company-math-symbols-latex t))
-
-  (add-hook 'global-company-mode-hook #'my--company-math)
-
-  :config
-  (add-to-list 'company-math-allow-latex-symbols-in-faces #'markdown-math-face))
-
-(use-package company-auctex
-  :defer t
-
-  :init
-  (defun my--company-auctex ()
-    (make-local-variable 'company-backends)
-    (company-auctex-init))
-
-  (add-hook 'LaTeX-mode-hook #'my--company-auctex))
 
 (provide 'conf/company)
