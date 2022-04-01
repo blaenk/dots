c57702e6f07c130efd353c5277de2ff4242476ce
Jorge Israel Peña <jorge.israel.p@gmail.com>
2022-03-31 21:07:54 -0700

    zsh: try mvnw if present, else mvn

--- zsh/zsh/sdkman.zsh
+++ zsh/zsh/sdkman.zsh
@@ -4,3 +4,20 @@ export SDKMAN_DIR="$HOME/.sdkman"
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+mvn-or-mvnw() {
+  local dir="$PWD"
+  while [[ ! -x "$dir/mvnw" && "$dir" != / ]]; do
+    dir="${dir:h}"
+  done
+
+  if [[ -x "$dir/mvnw" ]]; then
+    echo "Running \`$dir/mvnw\`..." >&2
+    "$dir/mvnw" "$@"
+    return $?
+  fi
+
+  command mvn "$@"
+}
+
+alias mvn="mvn-or-mvnw"
