1e4625d33dad7858402b6a65263b7dcd1429ddf4
Jorge Israel Peña <jorge.israel.p@gmail.com>
2023-08-03 02:33:43 -0700

    zsh: use cd completion for enhancd

--- zsh/zsh/enhancd.zsh
+++ zsh/zsh/enhancd.zsh
@@ -1,4 +1,13 @@
-export ENHANCD_HYPHEN_ARG=--
-export ENHANCD_DOT_ARG=...
+export ENHANCD_ENABLE_HYPHEN=false
+export ENHANCD_ENABLE_DOUBLE_DOT=false
+export ENHANCD_ENABLE_SINGLE_DOT=false
+export ENHANCD_ENABLE_HOME=true
+export ENHANCD_USE_ABBREV=true
+
+export ENHANCD_ARG_HYPHEN=--
+export ENHANCD_ARG_DOUBLE_DOT=...
+export ENHANCD_ARG_HOME=''
 export ENHANCD_DOT_SHOW_FULLPATH=1
 export ENHANCD_USE_FUZZY_MATCH=0
+
+compdef _cd __enhancd::cd
