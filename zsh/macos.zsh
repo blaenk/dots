0813a1605486639e0adaa5abd710d06d5a18b315
Jorge Israel Peña <jorge.israel.p@gmail.com>
2025-05-10 12:46:48 -0700

    Install missing brew apps, fix iterm2 integration

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -1,13 +1,3 @@
-if [[ -z "$MACOS" ]]; then
-  iterm2_prompt_mark() {}
-
-  iterm2_prompt_mark() {}
-
-  iterm2_prompt_end() {}
-
-  return 0
-fi
-
 iterm2_prompt_mark() {
   printf "\033]133;A\007"
 }
@@ -17,25 +7,49 @@ iterm2_prompt_end() {
   printf "\033]133;B\007"
 }
 
-export IS_MACOS=true
+# If this ever has problems, read https://gitlab.com/gnachman/iterm2/-/issues/10537
+# The alternative is to establish a trigger on the iTerm side to detect the prompt.
+#
+# It seems someone also managed to get it to work with a `[custom.iterm_prompt_mark]`
+# prompt component, but they may have forgotten to add the iterm_prompt_mark_end
+# component, which apparently cause cmd+shift+a to select the start of the prompt
+# export ITERM2_SQUELCH_MARK=1
 
-# ITERM_INTEGRATION="$DOTSPATH/zsh/zsh/iterm.zsh"
+ITERM_INTEGRATION="$DOTSPATH/zsh/iterm.zsh"
 
-# if [ ! -f "${ITERM_INTEGRATION}" ]; then
-#   curl -s -L 'https://iterm2.com/shell_integration/zsh' -o "${ITERM_INTEGRATION}"
+if [ ! -f "${ITERM_INTEGRATION}" ]; then
+  curl -s -L 'https://iterm2.com/shell_integration/zsh' -o "${ITERM_INTEGRATION}"
 
-#   # Apply iterm2_prompt_mark patch
-#   patch -s -d "$DOTSPATH/zsh/zsh/" < <(curl -s -L 'https://git.io/fhkwE')
-# fi
+  # Apply iterm2_prompt_mark patch
+  # patch -s -d "$DOTSPATH/zsh/" < <(curl -s -L 'https://git.io/fhkwE')
+fi
 
-# source "${ITERM_INTEGRATION}"
+source "${ITERM_INTEGRATION}"
 
 if ! command_exists brew; then
+  echo "Please install homebrew"
   return 0
 fi
 
-HB_CNF_HANDLER="/opt/homebrew/Library/Taps/homebrew/homebrew-command-not-found/handler.sh"
+command_exists atuin || brew install atuin
+command_exists eza || brew install eza
 
-if [ -f "$HB_CNF_HANDLER" ]; then
-  source "$HB_CNF_HANDLER";
-fi
+# Define a list of commands to check and install
+commands_to_check=(
+  atuin
+  bat
+  eza
+  fd
+  starship
+  zoxide
+)
+
+# Loop through the list of commands
+for cmd in "${commands_to_check[@]}"; do
+  # Check if the command exists
+  if ! command_exists "$cmd" ; then
+    echo "$cmd not found. Installing with brew..."
+    # Install the command using brew
+    brew install "$cmd"
+  fi
+done
