6cd5722358d61ca6c15bbd127de287dad0157964
Jorge Israel Peña <jorge.israel.p@gmail.com>
2025-05-10 12:45:58 -0700

    Check ostype at source time instead

--- zsh/linux.zsh
+++ zsh/linux.zsh
@@ -1,18 +1,16 @@
-if [[ "$OSTYPE" == linux* ]]; then
-  # nvidia fix
-  # export __GL_SYNC_TO_VBLANK=1
-  # export __GL_SYNC_DISPLAY_DEVICE=DFP-0
-  # export __VDPAU_NVIDIA_SYNC_DISPLAY_DEVICE=DFP-0
+# nvidia fix
+# export __GL_SYNC_TO_VBLANK=1
+# export __GL_SYNC_DISPLAY_DEVICE=DFP-0
+# export __VDPAU_NVIDIA_SYNC_DISPLAY_DEVICE=DFP-0
 
-  if (( $+commands[gnome-keyring-daemon] )); then
-    eval $(gnome-keyring-daemon -s)
-    export SSH_AUTH_SOCK
-  fi
+if (( $+commands[gnome-keyring-daemon] )); then
+  eval $(gnome-keyring-daemon -s)
+  export SSH_AUTH_SOCK
+fi
 
-  # systemctl --user env imports
-  # systemctl --user import-environment PATH SSH_AUTH_SOCK VM
+# systemctl --user env imports
+# systemctl --user import-environment PATH SSH_AUTH_SOCK VM
 
-  if [[ "$TERM" == xterm ]]; then
-    export TERM=xterm-256color
-  fi
+if [[ "$TERM" == xterm ]]; then
+  export TERM=xterm-256color
 fi
