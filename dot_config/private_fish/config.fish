306c2b775bb5512c3fbba294a1bfa5e20ddc1bbf
Jorge Israel Peña <jorge.israel.p@gmail.com>
2025-05-10 01:06:55 -0700

    Set some fish vi bindings config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,4 +1,35 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
+    fzf --fish | source
+    zoxide init --cmd cd fish | source
+    atuin init fish --disable-up-arrow | source
+
+    # Emulates vim's cursor shape behavior
+    # Set the normal and visual mode cursors to a block
+    set fish_cursor_default block
+    # Set the insert mode cursor to a line
+    set fish_cursor_insert block
+    # Set the replace mode cursors to an underscore
+    set fish_cursor_replace_one underscore
+    set fish_cursor_replace underscore
+    # Set the external cursor to a line. The external cursor appears when a command is started.
+    # The cursor shape takes the value of fish_cursor_default when fish_cursor_external is not specified.
+    set fish_cursor_external block
+    # The following variable can be used to configure cursor shape in
+    # visual mode, but due to fish_cursor_default, is redundant here
+    set fish_cursor_visual block
+
+    fish_vi_key_bindings
+end
+
+function fish_user_key_bindings
+  # Execute this once per mode that emacs bindings should be used in
+  fish_default_key_bindings -M insert
+
+  # Then execute the vi-bindings so they take precedence when there's a conflict.
+  # Without --no-erase fish_vi_key_bindings will default to
+  # resetting all bindings.
+  # The argument specifies the initial mode (insert, "default" or visual).
+  fish_vi_key_bindings --no-erase insert
 end
