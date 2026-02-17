884290475c0965843de992e035e48d18b6cdaf07
Jorge Israel Peña <jorgep@ziprecruiter.com>
2026-02-17 10:36:30 -0800

    Add fzf-tab config

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -22,6 +22,21 @@ zstyle ':fzf-tab:*' default-color $'\033[93m'
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
 zstyle ':fzf-tab:*' fzf-flags $FZF_TAB_FLAGS
 
+# disable sort when completing `git checkout`
+zstyle ':completion:*:git-checkout:*' sort false
+# set descriptions format to enable group support
+# NOTE: don't use escape sequences (like '%F{red}%d%f') here, fzf-tab will ignore them
+zstyle ':completion:*:descriptions' format '[%d]'
+# set list-colors to enable filename colorizing
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+zstyle ':completion:*' menu no
+# To make fzf-tab follow FZF_DEFAULT_OPTS.
+# NOTE: This may lead to unexpected behavior since some flags break this plugin. See Aloxaf/fzf-tab#455.
+zstyle ':fzf-tab:*' use-fzf-default-opts yes
+# switch group using `<` and `>`
+zstyle ':fzf-tab:*' switch-group '<' '>'
+
 # bindkey -M viins '^I^I' fzf-tab-complete
 
 # Use fd (https://github.com/sharkdp/fd) for listing path candidates.
